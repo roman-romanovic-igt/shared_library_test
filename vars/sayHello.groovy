45bcc34bdc2c09796e72e16c5694b58e596ec811
Romanovic, Roman <roman.romanovic@igt.com>
2025-11-28 11:40:32 +0100

    calling functions

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,8 +3,8 @@
 def call(String name = 'human') {
     echo "Hello, ${name}."
 
-    getCxCLI
-    untarAstCli
+    getCxCLI()
+    untarAstCli()
 }
 
 def getCxCLI(String urlString = 'http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz', String localPath = 'ast-cli_linux_x64.tar.gz') {
