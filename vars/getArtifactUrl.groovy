23560d13da44ff04ae12a5374e590927c242bb00
Romanovic, Roman <roman.romanovic@igt.com>
2025-11-28 10:50:42 +0100

    Added groovy script for downloading Cx CLI tool

---
+++ vars/getArtifactUrl.groovy
@@ -0,0 +1,12 @@
+def getCxCLI(String urlString = 'http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz', String localPath) {
+    def url = new URL(urlString)
+    def file = new File(localPath)
+
+    file.withOutputStream { out ->
+        url.withInputStream { input ->
+            out << input
+        }
+    }
+
+    println "Downloaded ${file.size()} bytes to ${localPath}"
+}
