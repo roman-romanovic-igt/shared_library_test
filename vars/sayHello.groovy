d48cad2ed790e8b623c355f2f902d839f1159a0e
Romanovic, Roman <roman.romanovic@igt.com>
2025-11-28 11:31:52 +0100

    Moving all to one script

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,8 +1,26 @@
 #!/usr/bin/env groovy
-evaluate(new File("./getArtifactUrl.groovy"))
 
 def call(String name = 'human') {
     echo "Hello, ${name}."
 
-    getArtifactUrl
+    getCxCLI
+    untarAstCli
+}
+
+def getCxCLI(String urlString = 'http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz', String localPath = 'ast-cli_linux_x64.tar.gz') {
+    def url = new URL(urlString)
+    def file = new File(localPath)
+
+    file.withOutputStream { out ->
+        url.withInputStream { input ->
+            out << input
+        }
+    }
+
+    println "Downloaded ${file.size()} bytes to ${localPath}"
+}
+
+def untarAstCli(String localPath = 'ast-cli_linux_x64.tar.gz') {
+    sh "tar -xzvf ${localPath}"
+    sh "./cx version"
 }
