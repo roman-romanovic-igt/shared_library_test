034f7c39dea7b1a2a7ec4974163236c0ae75ad72
Romanovic, Roman <roman.romanovic@igt.com>
2025-11-28 12:44:42 +0100

    baby steps 4

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -6,7 +6,7 @@ def call(String name="human") {
     downloadCxCLI('http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz')
 }
 
-def downloadCxCLI(String urlString, String localPath = "./ast-cli_linux_x64.tar.gz") {
+def downloadCxCLI(String urlString, String localPath = "/tmp/ast-cli_linux_x64.tar.gz") {
     def url = new URL(urlString)
     def file = new File(localPath)
 
