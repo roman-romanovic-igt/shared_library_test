1a9343b7baaa705d38f27013ed069819170251ee
Romanovic, Roman <roman.romanovic@igt.com>
2025-11-28 12:17:09 +0100

    roots bloody roots

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,13 +1,5 @@
 #!/usr/bin/env groovy
 
-def call() {
-    println "I am in the call function"
-
-    sh "wget -q http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz"
-    println "binary downloaded"
-
-    sh "tar -xzvf ast-cli_linux_x64.tar.gz"
-    println "ast-cli unpacked"
-
-    println "${./cx version}"
+def call(String name="human") {
+    echo "Hello, ${name}"
 }
