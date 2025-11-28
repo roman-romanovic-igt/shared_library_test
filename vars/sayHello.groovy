d97bbe201b81b5c50e5e1b15f2fabd489168f22e
Romanovic, Roman <roman.romanovic@igt.com>
2025-11-28 11:48:09 +0100

    blah blah blah

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -7,22 +7,17 @@ def call(String name = 'human') {
     untarAstCli()
 }
 
-def getCxCLI(String urlString = 'http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz', String localPath = './ast-cli_linux_x64.tar.gz') {
+def getCxCLI(String urlString = 'http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz') {
     def url = new URL(urlString)
     def file = new File(localPath)
 
-    println "I am in getCxCli"
+    sh "wget -q ${urlString}"
 
-    file.withOutputStream { out ->
-        url.withInputStream { input ->
-            out << input
-        }
-    }
-
-    println "Downloaded ${file.size()} bytes to ${localPath}"
+    println "Cx CLI downloaded"
 }
 
 def untarAstCli(String localPath = 'ast-cli_linux_x64.tar.gz') {
+    println "I am in untarAstCli"
     sh "tar -xzvf ${localPath}"
     sh "./cx version"
 }
