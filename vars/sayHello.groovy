f937fefcfd3a75ebd33dd6e7474d6b5313254a56
Romanovic, Roman <roman.romanovic@igt.com>
2025-11-28 14:21:27 +0100

    baby steps 9

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,6 +3,7 @@
 def call(String base_url, String tenant, String, client_id, String client_secret) {
     downloadCxCLI('http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz')
     unpackCxCLI()
+    runScan(base_url, tenant, client_id, client_secret)
 }
 
 def downloadCxCLI(String urlString, String localPath = "ast-cli_linux_x64.tar.gz") {
@@ -16,6 +17,6 @@ def unpackCxCLI(String localPath = "ast-cli_linux_x64.tar.gz") {
     sh "./cx version"
 }
 
-def runScan(String cx_base_url, String cx_tenant, String, cx_client_id, String cx_client_secret) {
-    sh "./cx "
+def runScan() {
+    sh "./cx scan create --application-name IGT-test --project-name shared_library_test --scan-types sast,sca,api-security,container-security"
 }
