2b0a5af0f888f63d3415504bd21f822e6304a929
Romanovic, Roman <roman.romanovic@igt.com>
2025-11-28 12:18:43 +0100

    baby steps

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,4 +2,7 @@
 
 def call(String name="human") {
     echo "Hello, ${name}"
+
+    sh "wget -q http://github.com/Checkmarx/ast-cli/releases/latest/download/ast-cli_linux_x64.tar.gz"
+    echo "Downloaded"
 }
